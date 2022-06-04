2db56c04f8bc4d508bff1e60a9ab88110a40254f
Zachary Lounsberry <zlounsberry@gmail.com>
2022-06-04 10:02:51 -0400

    menu WIP
    menu barebones structure in place

--- ui/main_menu/MainMenu.gd
+++ ui/main_menu/MainMenu.gd
@@ -1,5 +1,26 @@
 extends Control
 
+onready var animation_player = $AnimationPlayer
+
 func _ready():
+	animation_player.play("FadeOut")
+	var t = Timer.new()
+	t.set_wait_time(3)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	$HBoxContainer/Start.grab_focus()
+
+func _on_Start_pressed():
+	animation_player.play("FadeBlack")
+	yield(animation_player, "animation_finished")
+	get_tree().change_scene("res://main/Level1/Level1.tscn")
+	$AudioStreamPlayer.stop()
+
+func _on_Options_pressed():
 	pass # Replace with function body.
 
+
+func _on_Credits_pressed():
+	pass # Replace with function body.
